c39d3fd25d2bbef5032777fd0cb149b62ceb10e0
kdchurch1994 <kdchurch@bsu.edu>
2021-06-04 20:00:34 -0400

    added a function and worked with the console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!"); //provides an alert to anyone visiting the site that JavaScript is running. 
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("THis logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Out robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() { 
+    window.alert("The fight has begun!");
+}
+
+// fight();
